108781a1568b13cc90c63b98f21542547e3667bd
alexanderushe <alex.f.ushe@gmail.com>
2023-10-01 14:34:44 -0700

    sa

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,7 @@
-#!/usr/bin/python3
-
-"""
-pascal triangle.
-"""
-
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
     triangle = []
     for i in range(n):
         row = []
@@ -18,4 +12,5 @@ def pascal_triangle(n):
                 prev_row = triangle[i - 1]
                 row.append(prev_row[j - 1] + prev_row[j])
         triangle.append(row)
+
     return triangle
