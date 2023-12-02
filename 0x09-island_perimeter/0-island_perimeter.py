1e16ce2fa7fcb71588220f06b28b3dec84ae58e3
alexanderushe <alex.f.ushe@gmail.com>
2023-12-02 16:10:39 +0200

    added comments

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,21 +8,21 @@ def island_perimeter(grid):
     '''return perimeter of island
     '''
     perimeter = 0
-    #iterate through each cell in the grid
+    '''iterate through each cell in the grid'''
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            #checking for land cells
+            '''checking for land cells'''
             if grid[i][j] == 1:
-                #check top
+                '''check top'''
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                #check bottom cell
+                '''check bottom cell'''
                 if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                #check left cell
+                '''check left cell'''
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                #check right cell
+                '''check right cell'''
                 if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
